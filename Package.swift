58b0058a012dec6e7bb40418f2e601477cce4c01
Yusaku Nishi <jrsaruo@gmail.com>
2022-04-11 22:16:12 +0900

    update: specify platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "UIKitEssentials",
+    platforms: [
+        .iOS(.v9),
+        .tvOS(.v9)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
