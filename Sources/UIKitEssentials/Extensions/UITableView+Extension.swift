960db2b1b84832c3b6ebc9f978a4556837d69878
Yusaku Nishi <jrsaruo@gmail.com>
2023-03-11 04:41:57 +0900

    add: UITableView.deselectSelectedRows(with:animated:)

--- Sources/UIKitEssentials/Extensions/UITableView+Extension.swift
+++ Sources/UIKitEssentials/Extensions/UITableView+Extension.swift
@@ -24,6 +24,52 @@ extension UITableView {
         }
     }
     
+    /// Deselects all selected rows using the transition coordinator.
+    ///
+    /// Calling this method doesn’t cause the delegate to receive a `tableView(_:willDeselectRowAt:)` or `tableView(_:didDeselectRowAt:)` message,
+    /// nor does it send `selectionDidChangeNotification` notifications to observers.
+    ///
+    /// Calling this method doesn’t cause any scrolling to deselected rows.
+    ///
+    /// Typically, you can use this method in `viewWillAppear` of the view controller:
+    ///
+    /// ```
+    /// class ViewController: UIViewController {
+    ///
+    ///     override func viewWillAppear(_ animated: Bool) {
+    ///         super.viewWillAppear(animated)
+    ///
+    ///         tableView.deselectSelectedRows(with: transitionCoordinator,
+    ///                                        animated: animated)
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// If the animation is cancelled, rows will be reselected.
+    ///
+    /// - Parameters:
+    ///   - transitionCoordinator: A transition coordinator used to animate the deselection.
+    ///                            If `nil`, the deselection will be done with normal animation.
+    ///   - animated: `true` if you want to animate the deselection, and `false` if the change should be immediate.
+    public func deselectSelectedRows(with transitionCoordinator: UIViewControllerTransitionCoordinator?,
+                                     animated: Bool) {
+        guard let transitionCoordinator = transitionCoordinator else {
+            deselectSelectedRows(animated: animated)
+            return
+        }
+        transitionCoordinator.animate(alongsideTransition: { [weak self] _ in
+            self?.deselectSelectedRows(animated: animated)
+        }, completion: { [weak self] context in
+            guard let self,
+                  let indexPathsForSelectedRows = self.indexPathsForSelectedRows else { return }
+            if context.isCancelled {
+                for indexPathForSelectedRow in indexPathsForSelectedRows {
+                    self.selectRow(at: indexPathForSelectedRow, animated: animated, scrollPosition: .none)
+                }
+            }
+        })
+    }
+    
     /// Deselects the selected row using the transition coordinator.
     ///
     /// Calling this method doesn’t cause the delegate to receive a `tableView(_:willDeselectRowAt:)` or `tableView(_:didDeselectRowAt:)` message,
@@ -51,6 +97,7 @@ extension UITableView {
     ///   - transitionCoordinator: A transition coordinator used to animate the deselection.
     ///                            If `nil`, the deselection will be done with normal animation.
     ///   - animated: `true` if you want to animate the deselection, and `false` if the change should be immediate.
+    @available(*, deprecated, message: "Use deselectSelectedRows(with:animated:) instead.")
     public func deselectSelectedRow(with transitionCoordinator: UIViewControllerTransitionCoordinator?,
                                     animated: Bool) {
         guard let indexPathForSelectedRow = indexPathForSelectedRow else { return }
