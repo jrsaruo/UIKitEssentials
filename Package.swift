e95d66cd6130aceca51a12f32db13aec60497a1b
Yusaku Nishi <jrsaruo@gmail.com>
2024-06-30 20:52:35 +0900

    add: visionOS for supported platforms

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,8 @@ let package = Package(
     name: "UIKitEssentials",
     platforms: [
         .iOS(.v9),
-        .tvOS(.v9)
+        .tvOS(.v9),
+        .visionOS(.v1),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
